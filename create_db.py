a21dd2773448526b7bc47745f34f305e420c7ca0
Vaani Goenka <109366025+vaaniGO@users.noreply.github.com>
2024-12-19 22:22:04 +0530

    Update create_db.py

--- create_db.py
+++ create_db.py
@@ -1,21 +1,16 @@
-# from langchain.document_loaders import DirectoryLoader
+
 from langchain_community.document_loaders import DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.schema import Document
-# from langchain.embeddings import OpenAIEmbeddings
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from langchain_community.vectorstores import Chroma
-# import openai #Remove this
 from dotenv import load_dotenv
 import os
 import shutil
 import google.generativeai as genai # New Import
 
-# Load environment variables. Assumes that project contains .env file with API keys
 load_dotenv()
-#---- Set Gemini API key 
-# Change environment variable name from "OPENAI_API_KEY" to the name given in 
-# your .env file.
+# Set Gemini API key 
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 CHROMA_PATH = "chroma"
